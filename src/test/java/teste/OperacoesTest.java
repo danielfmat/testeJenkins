f9d9f384f23a300f9cd54f9dcbea6e5e05453ba9
danielfmat <daniel@egideti.com.br>
2015-06-10 18:20:52 -0300

    Criando projeto

---
+++ src/test/java/teste/OperacoesTest.java
@@ -0,0 +1,24 @@
+package teste;
+
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class OperacoesTest {
+	
+	@Test
+	public void somaTest(){
+		Operacoes op = new Operacoes(3,3);
+		int ret = op.soma();
+		Assert.assertEquals(6, ret);
+	}
+	
+	@Test
+	public void subTest(){
+		Operacoes op = new Operacoes(5,3);
+		int ret = op.subtracao();
+		Assert.assertEquals(2, ret);
+	}
+
+}
