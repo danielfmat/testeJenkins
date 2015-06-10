8d5c57f925b7e32c56bf4831786062e2b96b4839
Daniel Matsukuma <danielfmat@gmail.com>
2015-06-10 19:43:38 -0300

    add funcionalidade de teste

--- src/test/java/teste/OperacoesTest.java
+++ src/test/java/teste/OperacoesTest.java
@@ -23,4 +23,16 @@ public class OperacoesTest {
 		Assert.assertEquals(2, ret);
 	}
 
+			@Test
+	public void testFull(){
+		Operacoes op = new Operacoes(5,3);
+		int ret = op.subtracao();
+		//testeDaniel
+		Assert.assertEquals(2, ret);
+		Operacoes op = new Operacoes(3,3);
+		int ret = op.soma();
+		Assert.assertEquals(6, ret);
+	}
+
+
 }
