ea1ac636efaa78098adfeeef08c14ff55a9e218a
danielfmat <daniel@egideti.com.br>
2015-06-10 18:24:37 -0300

    primeira comentario

--- src/test/java/teste/OperacoesTest.java
+++ src/test/java/teste/OperacoesTest.java
@@ -8,16 +8,18 @@ import org.junit.Test;
 public class OperacoesTest {
 	
 	@Test
-	public void somaTest(){
+	public void somaTest(){		
 		Operacoes op = new Operacoes(3,3);
 		int ret = op.soma();
 		Assert.assertEquals(6, ret);
 	}
 	
+	
 	@Test
 	public void subTest(){
 		Operacoes op = new Operacoes(5,3);
 		int ret = op.subtracao();
+		//testeDaniel
 		Assert.assertEquals(2, ret);
 	}
 
